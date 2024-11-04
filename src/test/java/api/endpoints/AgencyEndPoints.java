a8bd82fac9510d469b8a97d6fbba06c436a3ebd9
sagarraipurkar <er.sagarraipurkar@gmail.com>
2024-11-04 10:37:57 +0530

    Added dependency in the pom.xml file for jenkins

--- src/test/java/api/endpoints/AgencyEndPoints.java
+++ src/test/java/api/endpoints/AgencyEndPoints.java
@@ -25,28 +25,27 @@ import static io.restassured.RestAssured.given;
 public class AgencyEndPoints {
 	static TokenGenerate generate = new TokenGenerate();
 	static String Token = generate.Bearertoken();
-	
+
 	static ResourceBundle getURL() {
-	ResourceBundle routes =  ResourceBundle.getBundle("routes");
-	return routes;
+		ResourceBundle routes = ResourceBundle.getBundle("routes");
+		return routes;
 	}
-	
+
 	public static Response CreateAgency() throws FileNotFoundException {
-		String Post_url =  getURL().getString("Post_url");
-		File f = new File(
-				"C:\\Eclipse New\\NewWorkSpace\\TravelCrmApiAutomation\\testdata\\CreateAgencyTestData.json");
+		String Post_url = getURL().getString("Post_url");
+		File f = new File("C:\\Eclipse New\\NewWorkSpace\\TravelCrmApiAutomation\\testdata\\CreateAgencyTestData.json");
 		FileReader fr = new FileReader(f);
 		JSONTokener jsonToken = new JSONTokener(fr);
 		JSONObject data = new JSONObject(jsonToken);
-		Response response =given().header("Authorization", "Bearer " + Token)
-				.contentType(ContentType.JSON).body(data).when().post(Post_url);
+		Response response = given().header("Authorization", "Bearer " + Token).contentType(ContentType.JSON).body(data)
+				.when().post(Post_url);
 		return response;
 	}
 
 	public static Response GetAgency() {
-		String get_url =  getURL().getString("get_url");
-		Response response = given().header("Authorization", "Bearer " + Token).pathParam("agencyId", 1).contentType(ContentType.JSON).when()
-				.get(get_url);
+		String get_url = getURL().getString("get_url");
+		Response response = given().header("Authorization", "Bearer " + Token).pathParam("agencyId", 1)
+				.contentType(ContentType.JSON).when().get(get_url);
 		return response;
 	}
 
